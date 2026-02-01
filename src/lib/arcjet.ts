22b955f7873d693337a18be5f081ae99f90f1583
Abhijit47 <akarmakar846@gmail.com>
2026-02-01 11:56:14 +0530

    13-feat. Added arcjet with basic rules

---
+++ src/lib/arcjet.ts
@@ -0,0 +1,28 @@
+import arcjet, {
+  detectBot,
+  fixedWindow,
+  protectSignup,
+  sensitiveInfo,
+  shield,
+  slidingWindow,
+  tokenBucket,
+} from '@arcjet/next';
+
+// Re-export the rules to simplify imports inside handlers
+export {
+  detectBot,
+  fixedWindow,
+  protectSignup,
+  sensitiveInfo,
+  shield,
+  slidingWindow,
+  tokenBucket,
+};
+
+// Create a base Arcjet instance for use by each handler
+export default arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    // You could include one or more base rules to run on every request here.
+  ],
+});
