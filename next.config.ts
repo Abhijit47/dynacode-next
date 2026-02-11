8a93e44f0014d7240038eb9e728ccb7923e0f65d
Abhijit47 <akarmakar846@gmail.com>
2026-02-11 12:18:14 +0530

    21:feat.-Added CSP using Subresource Integrity (SRI)

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,70 @@
 import type { NextConfig } from 'next';
+// const isDev = process.env.NODE_ENV === 'development';
+// const cspHeader = `
+//     default-src 'self';
+//     script-src 'self' 'unsafe-inline'${isDev ? " 'unsafe-eval'" : ''};
+//     style-src 'self' 'unsafe-inline';
+//     img-src 'self' blob: data:;
+//     font-src 'self';
+//     object-src 'none';
+//     base-uri 'self';
+//     form-action 'self';
+//     frame-ancestors 'none';
+//     upgrade-insecure-requests;
+// `;
+
+// CSP configuration with SRI
+const cspHeader = `
+    default-src 'self';
+    script-src 'self';
+    style-src 'self';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        // Apply this policy to all routes
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value:
-              "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;",
-          },
-          // You can explicitly disable the old header if desired
-          {
-            key: 'X-XSS-Protection',
-            value: '0',
-          },
-          // Recommended: Add other modern security headers
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-        ],
-      },
-    ];
-  },
+  // without nonce
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/(.*)',
+  //       headers: [
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: cspHeader.replace(/\n/g, ''),
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
   /* config options here */
   reactCompiler: true,
   typedRoutes: true,
   experimental: {
     typedEnv: true,
+    sri: {
+      algorithm: 'sha256',
+    },
+  },
+
+  // CSP configuration with SRI
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
   },
 
   images: {
